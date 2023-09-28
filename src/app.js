c2609423a580461ca2834b51e96a8acdd4d1fe7b
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-09-28 20:20:49 +0000

    se arreglo el array mal definido

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ let noun = ["jogger", "racoon"];
 let domain = [".com", ".net", ".us", ".io"];
 
 for (const pronouns of pronoun) {
-  for (const abjs of abj) {
+  for (const abjs of adj) {
     for (const nouns of noun) {
       for (const domains of domain) {
-        console.log(pronouns + abjs + nouns + domains);
+
+        console.log(pronouns + abjs + nouns + domains );
       }
     }
   }
